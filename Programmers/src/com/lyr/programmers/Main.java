879d955d048d7a9683c81185e441c5f2ab25e733
이예린 <yerin102@nate.com>
2022-02-15 21:29:44 +0900

    커밋계정변경

--- Programmers/src/com/lyr/programmers/Main.java
+++ Programmers/src/com/lyr/programmers/Main.java
@@ -1,12 +1,49 @@
 package com.lyr.programmers;
 
 import com.lyr.programmers.level1.LoggingRectangleStar;
+import com.lyr.programmers.level1.Lotto;
+import com.lyr.programmers.level1.AddingDigit;
+import com.lyr.programmers.level1.AllCasesOfSum;
 import com.lyr.programmers.level1.ArrayAverage;
+import com.lyr.programmers.level1.AtWillStringOrder;
+import com.lyr.programmers.level1.Budget;
+import com.lyr.programmers.level1.CaesarCipher;
+import com.lyr.programmers.level1.CalculatingInsufficientCash;
+import com.lyr.programmers.level1.ClawCraneGame;
 import com.lyr.programmers.level1.CollatzConjecture;
+import com.lyr.programmers.level1.DealingWithString;
+import com.lyr.programmers.level1.DescendingOrder;
+import com.lyr.programmers.level1.EvenAndOdd;
+import com.lyr.programmers.level1.FailureRate;
+import com.lyr.programmers.level1.FindingKimInSeoul;
+import com.lyr.programmers.level1.FindingPrimeNumber;
+import com.lyr.programmers.level1.GettingMiddleLetter;
+import com.lyr.programmers.level1.GettingWeekend;
+import com.lyr.programmers.level1.GreatestCommonDivisorAndLeastCommonMultiple;
 import com.lyr.programmers.level1.HarshadNumber;
 import com.lyr.programmers.level1.HidingPhoneNumber;
 import com.lyr.programmers.level1.MatrixAddition;
+import com.lyr.programmers.level1.MinimumRectangle;
+import com.lyr.programmers.level1.MockTest;
+import com.lyr.programmers.level1.Multiple;
 import com.lyr.programmers.level1.Multiplication;
+import com.lyr.programmers.level1.NaturalNumberToReverseArray;
+import com.lyr.programmers.level1.PAndYCount;
+import com.lyr.programmers.level1.PlayerWhoCouldNotComplete;
+import com.lyr.programmers.level1.Reaminder;
+import com.lyr.programmers.level1.RecommendingNewId;
+import com.lyr.programmers.level1.RemovingDuplication;
+import com.lyr.programmers.level1.RemovingSmallestNumber;
+import com.lyr.programmers.level1.ReverseTernary;
+import com.lyr.programmers.level1.ScalarProduct;
+import com.lyr.programmers.level1.SquareRootDiscrimination;
+import com.lyr.programmers.level1.StrangeString;
+import com.lyr.programmers.level1.StringDescendingOrder;
+import com.lyr.programmers.level1.SumBetweenTwoIntegers;
+import com.lyr.programmers.level1.SumOfDivisor;
+import com.lyr.programmers.level1.SumOfNegativeAndPositive;
+import com.lyr.programmers.level1.SumOfNotExistsNumber;
+import com.lyr.programmers.level1.Watermelon;
 
 /*
  * 패키지명 규칙 : com.[회사명].[프로젝트명].~
@@ -35,9 +72,180 @@ public class Main {
 //		CollatzConjecture s = new CollatzConjecture();
 //		s.solution(1);
 		
-		ArrayAverage s = new ArrayAverage();
-		int[] arr = {1,2,3,4};
-		s.solution(arr);
+//		ArrayAverage s = new ArrayAverage();
+//		int[] arr = {1,2,3,4};
+//		s.solution(arr);
+		
+//		GreatestCommonDivisorAndLeastCommonMultiple s = new GreatestCommonDivisorAndLeastCommonMultiple();
+//		s.solution(3, 12); 
+		
+//		EvenAndOdd s = new EvenAndOdd();
+//		System.out.println(s.solution(12));
+		
+//		RemovingSmallestNumber s = new RemovingSmallestNumber();
+//		int[] a = {4,3,2,1};
+//		int[] b = {10};
+//		s.solution(a);
+//		s.solution(b);
+		
+//		SquareRootDiscrimination s = new SquareRootDiscrimination();
+//		s.solution(122);
+		
+//		DescendingOrder s = new DescendingOrder();
+//		s.solution(118372);
+		
+//		NaturalNumberToReverseArray s = new NaturalNumberToReverseArray();
+//		s.solution(1231231);
+		
+//		StrangeString s = new StrangeString();
+//		s.solution("try hello world");
+		
+//		AddingDigit s = new AddingDigit();
+//		s.solution(19203);
+		
+//		SumOfDivisor s = new SumOfDivisor();
+//		s.solution(5);
+		
+//		CaesarCipher s = new CaesarCipher();
+//		s.solution("a B z", 4);
+		
+//		Watermelon s = new Watermelon();
+//		s.solution(3);
+//		s.solution(4);
+		
+//		FindingPrimeNumber s = new FindingPrimeNumber();
+//		s.solution(10);
+		
+//		FindingKimInSeoul s = new FindingKimInSeoul();
+//		String[] seoul = {"Jane", "Kim"};
+//		s.solution(seoul);
+		
+//		DealingWithString s = new DealingWithString();
+//		s.solution("22223");
+		
+//		StringDescendingOrder s = new StringDescendingOrder();
+//		s.solution("Zbcdefg");
+		
+//		PAndYCount s = new PAndYCount();
+//		s.solution("pPoooyY");
+		
+//		AtWillStringOrder s = new AtWillStringOrder();
+//		String[] strings = {"cdx", "abce", "abcd"};
+//		s.solution(strings, 1);
+		
+//		SumBetweenTwoIntegers s = new SumBetweenTwoIntegers();
+//		s.solution(5, 3);
+		
+//		Multiple s = new Multiple();
+//		int[] arr1 = {3, 2, 6};
+//		int[] arr2 = {5, 9, 7, 10};
+//		s.solution(arr1, 10);
+//		s.solution(arr2, 5);
+		
+//		GettingMiddleLetter s = new GettingMiddleLetter();
+//		s.solution("abcde"); //c
+//		s.solution("qwer"); //we
+//		s.solution("abcdef"); //cd
+		
+//		RemovingDuplication s = new RemovingDuplication();
+//		int[] arr1 = {1, 1, 3, 3, 0, 1, 1};
+//		s.solution(arr1);
+		
+//		CalculatingInsufficientCash s = new CalculatingInsufficientCash();
+//		s.solution(2500, 1000000000, 2500);
+		
+//		Reaminder s = new Reaminder();
+//		s.solution(10);
+//		s.solution(12);
+		
+//		MinimumRectangle s = new MinimumRectangle();
+//		int[][] arr1 = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+//		int[][] arr2 = {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}};
+//		s.solution(arr1);
+//		s.solution(arr2);
+		
+//		GettingWeekend s = new GettingWeekend();
+//		s.solution(5, 24);
+//		s.solution(1, 2);
+		
+//		AllCasesOfSum s = new AllCasesOfSum();
+//		int[] arr = {2, 1, 3, 4, 1};
+//		s.solution(arr);
+		
+//		ReverseTernary s = new ReverseTernary();
+//		s.solution(45);
+//		s.solution(125);
+		
+//		MockTest s = new MockTest();
+//		int[] arr1 = {1, 2, 3, 4, 5};
+//		int[] arr2 = {1, 3, 2, 4, 2};
+//		s.solution(arr1);
+//		s.solution(arr2);
+		
+//		ScalarProduct s = new ScalarProduct();
+//		int[] arr1 = {1, 2, 3, 4};
+//		int[] arr2 = {-3, -1, 0, 2};
+//		s.solution(arr1, arr2);
+		
+//		PlayerWhoCouldNotComplete s = new PlayerWhoCouldNotComplete();
+//		String[] participant = {"mislav", "stanko", "mislav", "ana"};
+//		String[] completion = {"stanko", "ana", "mislav"};
+		
+//		String[] participant = {"a", "b", "c", "c"};
+//		String[] completion = {"a", "b", "c"};
+//		s.solution(participant, completion);
+		
+//		SumOfNegativeAndPositive s = new SumOfNegativeAndPositive();
+//		int[] absolutes = {4, 7, 12};
+//		boolean[] signs = {true, false, true};
+//		s.solution(absolutes, signs);
+		
+//		SumOfNotExistsNumber s = new SumOfNotExistsNumber();
+//		int[] arr = {1, 2, 3, 4, 6, 7, 8, 0};
+//		s.solution(arr);
+		
+//		ClawCraneGame s = new ClawCraneGame();
+//		int[][] board = {{0,0,0,0,0}, {0,0,1,0,3}, {0,2,5,0,1}, {4,2,4,4,2}, {3,5,1,3,1}}; 
+//		int[] moves = {1, 5, 3, 5, 1, 2, 1, 4}; //4
+//		//쌓인 모습
+////		[0, 0, 0, 0, 0]
+////		[0, 0, 0, 0, 0]
+////		[0, 0, 5, 0, 0]
+////		[0, 2, 4, 4, 2]
+////		[0, 5, 1, 3, 1]
+//		
+//		s.solution(board, moves);
+		
+//		Budget s = new Budget();
+//		int[] d1 = {1, 3, 2, 5, 4};
+//		int budget1 = 9;
+//		int[] d2 = {2, 2, 3, 3};
+//		int budget2 = 10;
+//		s.solution(d1, budget1);
+//		s.solution(d2, budget2);
+		
+//		RecommendingNewId s = new RecommendingNewId();
+////		String newId = "a...a";
+////		String newId = "...=.=.=.=.=.=.=.b=.=E.=.=.=.=.=.=.=.=.=.=.=.=.=.x=.=.=....";
+//		String newId = "=.=";
+//		s.solution(newId);
+		
+//		Lotto s = new Lotto();
+//		int[] lottos1 = {44, 1, 0, 0, 31, 25};
+//		int[] winNums1 = {31, 10, 45, 1, 6, 19};
+//		int[] lottos2 = {0, 0, 0, 0, 0, 0};
+//		int[] winNums2 = {38, 19, 20, 40, 15, 25};
+//		s.solution(lottos1, winNums1);
+//		s.solution(lottos2, winNums2);
+		
+		FailureRate s = new FailureRate();
+		int stageCnt1 = 5; //스테이지 갯수
+		int[] stages1 = {2, 1, 2, 6, 2, 4, 3, 3}; //게임 사용자들이 현재 멈춰있는 스테이지 번호들
+		int stageCnt2 = 5;
+		int[] stages2 = {4, 4, 4, 4}; 
+//		s.solution(stageCnt1, stages1);
+		s.solution(stageCnt2, stages2);
+	
 	}
 
 }
